591ab8c75a2dbf7e6d98b83a0fd7b2834ec0051a
Rohit Bhatia <irohitbhatia@gmail.com>
2020-02-10 23:20:44 +0530

    feat: Added clear props to clear input

--- index.js
+++ index.js
@@ -7,35 +7,45 @@ import {View, TextInput, StyleSheet} from 'react-native'
 class NextTextInput extends PureComponent {
 	constructor(props) {
 		super(props)
-		const noOfRefs = this.props.noOfTextInput
-		this.inputRefs = Array(noOfRefs)
+		this.noOfRefs = this.props.noOfTextInput
+		this.inputRefs = Array(this.noOfRefs)
 			.fill(0)
 			.map(_ => React.createRef())
 		this.value = this.props.value
 		this.state = {
-			inputValue: Array(noOfRefs).fill(null).map((value, index) => this.value && this.value[index] !== undefined ? '' + this.value[index] : value)
+			inputValue: Array(this.noOfRefs).fill(null).map((value, index) => this.value && this.value[index] !== undefined ? '' + this.value[index] : value)
 		}
 	}
 
 
 
 	onChangeHandler = async (value, index) => {
+		const {clearInput} = this.props
 		const {inputValue} = this.state
 		inputValue[index] = value
 		await this.setState({inputValue: [...inputValue]})
 		this.props.onChangeValue(this.state.inputValue, this.state.inputValue[index], this.inputRefs[index])
 		const ref = this.inputRefs[index + 1]
-		if (value && ref && ref.current) ref.current.focus()
+		if (value && ref && ref.current && !clearInput) ref.current.focus()
+				
+	}
+
+	componentDidUpdate() {
+		const {clearInput} = this.props
+		let inputValues = false 
+		for (let i = 0; i<this.state.inputValue.length; i++) {
+			if (this.state.inputValue[i])  inputValues = true
+			if (inputValues) break
+		}
+		if (clearInput && inputValues) {
+			// if clear input is true and there is nothing in the first input 
+			this.setState({inputValue: Array(this.noOfRefs).fill(null)})
+			const ref = this.inputRefs[0]
+			if (ref && ref.current) ref.current.focus()	
+		}
 	}
 
 	render() {
-		const {clearInput} = this.props
-		if (clearInput && this.state.inputValue[0]) {
-			// if clear input is true and there is nothing in the first input 
-			this.state = {inputValue: Array(noOfRefs).fill(null)}
-			this.inputRefs = Array(noOfRefs).fill(0).map(_ => React.createRef())
-		}
-		
 		return (
 			<View
 				style={
